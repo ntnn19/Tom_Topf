961e8ff576e95d045e9adbaabdd6fd53692420f2
ntnn19 <natannagar89@gmail.com>
2024-06-15 22:11:12 +0200

    added colabfold search to workflow

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python "workflow/scripts/create_dirs.py" "config/config.yaml"
-snakemake --snakefile workflow/Snakefile  --use-singularity --singularity-args "--nv -B /home/nnagar/singularity_containers/colabfold/weights:/cache -B $(pwd)/workflow/results:/predictions"
+snakemake --snakefile workflow/Snakefile  --use-singularity --singularity-args "--nv -B /home/nnagar/singularity_containers/colabfold/weights:/cache -B $(pwd)/workflow/results:/predictions" -n
