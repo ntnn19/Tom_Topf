48aaed4e01b8c66dd495671242616268db7c6919
ntnn19 <natannagar89@gmail.com>
2024-06-15 10:48:20 +0200

    modified for support of mounting during runtime

--- workflow/scripts/create_dirs.py
+++ workflow/scripts/create_dirs.py
@@ -7,8 +7,8 @@ def main(config_file):
     conf = yaml.safe_load(Path(config_file).read_text())
     DATA_DIR = conf["data_dir"]
     RESULTS_DIR = conf["output_dir"]
-    os.makedirs("../../"+DATA_DIR, exist_ok=True)
-    os.makedirs("../../"+RESULTS_DIR, exist_ok=True)
+    os.makedirs(DATA_DIR, exist_ok=True)
+    os.makedirs(RESULTS_DIR, exist_ok=True)
 
 if __name__ == '__main__':
     import yaml
