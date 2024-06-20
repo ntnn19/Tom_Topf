5c51f052ebdf58f929567057d87b7eb8334ba041
ntnn19 <natannagar89@gmail.com>
2024-06-20 09:23:29 +0200

    pretified docs

--- run_workflow.sh
+++ run_workflow.sh
@@ -3,4 +3,4 @@
 colabfold_weights_dir=$1
 python "workflow/scripts/create_dirs.py" "config/config.yaml" $colabfold_weights_dir
 
-snakemake --snakefile workflow/Snakefile  --config muscle-params="${colabfold_weights_dir}" --use-singularity --singularity-args "--nv -B ${colabfold_weights_dir}:/cache -B $(pwd)/workflow/results/strategy-1/hsv-1/multi:/predictions" -c12 -k
+snakemake --snakefile workflow/Snakefile  --config colabfold_weights_dir="${colabfold_weights_dir}" --use-singularity --singularity-args "--nv -B ${colabfold_weights_dir}:/cache -B $(pwd)/workflow/results/strategy-1/hsv-1/multi:/predictions" -c12 -k
